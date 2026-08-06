567f2b4a78b71ce2d44fa1713be953f8a439b64e
yohendry <66956759+knxdark@users.noreply.github.com>
2026-08-05 22:42:20 -0400

    Fix: Add ProfessorsModule import to SectionsModule for ProfessorsService injection

--- Backend/src/modules/sections/sections.module.ts
+++ Backend/src/modules/sections/sections.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SectionsService } from './service';
 import { SectionsController } from './controller';
+import { ProfessorsModule } from '../professors/professors.module';
 
 @Module({
+  imports: [ProfessorsModule],
   controllers: [SectionsController],
   providers: [SectionsService],
   exports: [SectionsService],
